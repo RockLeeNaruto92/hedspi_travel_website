e5c5e19b8d7586c1dc5ab4550df5fbf9725c3bdc
Bui Minh Thu <buiminhthuk55@gmail.com>
2016-04-11 00:46:06 +0900

    view hotel detail

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -2,5 +2,9 @@ class HotelsController < ApplicationController
   def index
     @hotels = HotelServices.get_all_hotels
     @places = TravelServices.get_all_places
+
+    if params[:code].present?
+      @hotel = HotelServices.get_hotel_by_code params[:code]
+    end
   end
 end
--- app/services/hotel_services.rb
+++ app/services/hotel_services.rb
@@ -12,5 +12,14 @@ class HotelServices
         []
       end
     end
+
+    def get_hotel_by_code code
+      begin
+        result = client.call :find_hotel_by_code, message: {code: code}
+        JSON.parse result.to_hash[:find_hotel_by_code_response][:value]
+      rescue
+        {}
+      end
+    end
   end
 end
