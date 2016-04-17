704cea39c2adf8c133eb51095e34f8943514c88f
Bui Minh Thu <buiminhthuk55@gmail.com>
2016-04-18 02:15:57 +0900

    create success

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,36 @@
 class BookingsController < ApplicationController
   def index
+    load_info
+  end
+
+  def create
+    load_info
+    set_check_in_out_date
+
+    result = BookingServices.booking params[:bookings]
+
+    if result
+      redirect_to bookings_path({info: :success})
+    else
+      render :index
+    end
+  end
+
+  private
+  def load_info
     @places = TravelServices.get_all_places
     @hotels = HotelServices.get_all_hotels
     @airlines = AirlineServices.get_all_airlines
 
     @tours = TravelServices.get_all_tours
     @flights = AirlineServices.get_all_flights
+  end
+
+  def set_check_in_out_date
+    tour = @tours.select{|tour| tour["id"] == params[:bookings][:tourId].to_i}.first
+    if tour
+      params[:bookings][:checkInDate] = tour["start_date"]
+      params[:bookings][:checkOutDate] = (tour["start_date"].to_date + 5.days).to_s
+    end
   end
 end
---
+++ app/services/booking_services.rb
@@ -0,0 +1,16 @@
+class BookingServices
+  class << self
+    def client
+      @client ||= Savon::Client.new wsdl: "http://localhost:9090/ode/processes/Booking?wsdl"
+    end
+
+    def booking params
+      begin
+        result = client.call :process, message: params
+        result.to_hash[:booking_response][:result]
+      rescue
+        false
+      end
+    end
+  end
+end
