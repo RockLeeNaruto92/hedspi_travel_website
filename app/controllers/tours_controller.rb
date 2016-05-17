5a3abf93a675559ef309367f1ad26ad41f917504
Bui Minh Thu <buiminhthuk55@gmail.com>
2016-05-18 00:31:25 +0900

    add link 2

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,8 +1,4 @@
 class ToursController < ApplicationController
-  def index
-
-  end
-
   def show
     @tour = TravelServices.get_tour_by_code params[:id]
     @images = Image.of_place @tour["place_id"]
