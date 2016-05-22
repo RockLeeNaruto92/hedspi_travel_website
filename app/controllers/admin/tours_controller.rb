cbe4f8645a711a36cbc89c687b87936c33d286ed
Bui Minh Thu <buiminhthuk55@gmail.com>
2016-05-22 19:00:25 +0900

    update tour

--- app/controllers/admin/tours_controller.rb
+++ app/controllers/admin/tours_controller.rb
@@ -28,12 +28,28 @@ class Admin::ToursController < Admin::BaseController
     end
   end
 
-  private
-  def standardlize_tour_params
-    params[:tour].each do |key, val|
-      params[:tour][key.to_s.camelize(:lower)] = params[:tour][key]
-      params[:tour].delete key
+  def edit
+    @places = TravelServices.get_all_places
+    @tour = Tour.new (TravelServices.get_tour_by_id params[:id])
+  end
+
+  def update
+    @tour = Tour.new params[:tour]
+    @places = TravelServices.get_all_places
+    result = TravelServices.update_tour params[:tour]
+    if result
+      if result == "Successfully!"
+        flash[:success] = "Successfully!"
+        redirect_to admin_tours_path
+      else
+        flash[:danger] = "Failure!"
+        @tour.errors[:warning] = JSON.parse result
+        render "new"
+      end
+    else
+      flash[:danger] = "Failure!"
+      @tour.errors[:connection] = t "admin.places.errors.connection"
+      render "new"
     end
-    params
   end
 end
