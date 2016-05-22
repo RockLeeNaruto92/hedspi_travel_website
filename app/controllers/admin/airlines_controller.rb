6cd5fc69f4bcec60cc62d7b643238f98471a4fec
Bui Minh Thu <buiminhthuk55@gmail.com>
2016-05-22 17:55:15 +0900

    new airline

--- app/controllers/admin/airlines_controller.rb
+++ app/controllers/admin/airlines_controller.rb
@@ -2,4 +2,27 @@ class Admin::AirlinesController < Admin::BaseController
   def index
     @airlines = AirlineServices.get_all_airlines
   end
+
+  def new
+    @airline = Airline.new
+  end
+
+  def create
+    @airline = Airline.new params[:airline]
+    result = AirlineServices.add_new_airline params[:airline]
+    if result
+      if result == "Successfully!"
+        flash[:success] = "Successfully!"
+        redirect_to admin_airlines_path
+      else
+        flash[:danger] = "Failure!"
+        @airline.errors[:warning] = JSON.parse result
+        render "new"
+      end
+    else
+      flash[:danger] = "Failure!"
+      @airline.errors[:connection] = t "admin.airlines.errors.connection"
+      render "new"
+    end
+  end
 end
