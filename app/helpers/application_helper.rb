063144f8f1bd9e73069480cc06c0e8dd444d35d9
Bui Minh Thu <buiminhthuk55@gmail.com>
2016-05-25 01:21:54 +0900

    fix-bookings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def content_head content
     content
   end
+
+  def get_booking_params params, param_name
+    return params[:bookings][param_name] if params[:bookings].present?
+  end
 end
